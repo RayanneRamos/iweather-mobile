8254a55840668f7a1f0fb0022d1982f3034e3dd9
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-09-25 14:55:43 -0300

    testID

---
+++ src/components/Input/input.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react-native";
+import { Input } from "./index";
+
+describe("Component: Input", () => {
+  it("should be render without activity indicator if isLoading prop is undefined.", () => {
+    render(<Input isLoading />);
+
+    const activityIndicator = screen.getByTestId("activity-indicator");
+  });
+});
