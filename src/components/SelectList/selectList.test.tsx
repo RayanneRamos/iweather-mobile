e2e55b753671995dbf4265dafec86f2f91477d33
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-09-25 15:29:50 -0300

    mock de função

--- src/components/SelectList/selectList.test.tsx
+++ src/components/SelectList/selectList.test.tsx
@@ -7,10 +7,13 @@ describe("Component: SelectList", () => {
       { id: "1", name: "Rio de Janeiro", latitude: 123, longitude: 456 },
       { id: "2", name: "São Paulo", latitude: 789, longitude: 987 },
     ];
+    const onPress = jest.fn();
 
-    render(<SelectList data={data} onChange={() => {}} onPress={() => {}} />);
+    render(<SelectList data={data} onChange={() => {}} onPress={onPress} />);
 
     const selectedCity = screen.getByText(/rio/i);
     fireEvent.press(selectedCity);
+
+    expect(onPress).toBeCalledTimes(1);
   });
 });
