d9043a46263b1f969a75db9e642676accfa768ce
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-09-25 15:03:27 -0300

    toBeTruthy

--- src/components/Input/input.test.tsx
+++ src/components/Input/input.test.tsx
@@ -8,4 +8,11 @@ describe("Component: Input", () => {
     const activityIndicator = screen.queryByTestId("activity-indicator");
     expect(activityIndicator).toBeNull();
   });
+
+  it("shoudl be render with activity indicator if isLoading prop is true", () => {
+    render(<Input isLoading />);
+
+    const activityIndicator = screen.getByTestId("activity-indicator");
+    expect(activityIndicator).toBeTruthy();
+  });
 });
