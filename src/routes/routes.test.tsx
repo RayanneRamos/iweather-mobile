2c6c143158d6134c79f4744ab45113a09ca8426e
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-09-26 15:24:05 -0300

    finalizando o teste das rotas

--- src/routes/routes.test.tsx
+++ src/routes/routes.test.tsx
@@ -1,6 +1,9 @@
-import { render, screen, waitFor } from "../../__tests__/utils/customRender";
+import { render } from "../../__tests__/utils/customRender";
 import { Routes } from ".";
 import { saveStorageCity } from "@libs/asyncStorage/cityStorage";
+import { api } from "@services/api";
+import { mockWeatherAPIResponse } from "@__tests__/mocks/api/mockWeatherAPIResponse";
+import { act, screen, waitFor } from "@testing-library/react-native";
 
 describe("Routes", () => {
   it("should be render Search scrren when not city selected.", async () => {
@@ -11,15 +14,20 @@ describe("Routes", () => {
   });
 
   it("shoud be render Dashboard screen when has city selected", async () => {
+    jest.spyOn(api, "get").mockResolvedValue({ data: mockWeatherAPIResponse });
+
     const city = {
       id: "1",
-      name: "Rio de Janeiro",
+      name: "São Paulo",
       latitude: 123,
       longitude: 456,
     };
 
     await saveStorageCity(city);
+    await act(() => waitFor(() => render(<Routes />)));
 
-    render(<Routes />);
+    const title = screen.getByText(city.name);
+
+    expect(title).toBeTruthy();
   });
 });
