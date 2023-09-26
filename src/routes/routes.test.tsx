7919fc2e464be55c488af537d694b04f84ef9992
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-09-26 15:15:07 -0300

    customRender

--- src/routes/routes.test.tsx
+++ src/routes/routes.test.tsx
@@ -1,5 +1,6 @@
-import { render, screen, waitFor } from "@testing-library/react-native";
+import { render, screen, waitFor } from "../../__tests__/utils/customRender";
 import { Routes } from ".";
+import { saveStorageCity } from "@libs/asyncStorage/cityStorage";
 
 describe("Routes", () => {
   it("should be render Search scrren when not city selected.", async () => {
@@ -7,5 +8,18 @@ describe("Routes", () => {
 
     const title = await waitFor(() => screen.findByText(/^escolha um local/i));
     expect(title).toBeTruthy();
+  });
+
+  it("shoud be render Dashboard screen when has city selected", async () => {
+    const city = {
+      id: "1",
+      name: "Rio de Janeiro",
+      latitude: 123,
+      longitude: 456,
+    };
+
+    await saveStorageCity(city);
+
+    render(<Routes />);
   });
 });
