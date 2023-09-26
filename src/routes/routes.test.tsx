5cb8c2129c708875f086da467917828c6782388c
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-09-26 15:26:51 -0300

    fix

--- src/routes/routes.test.tsx
+++ src/routes/routes.test.tsx
@@ -1,9 +1,13 @@
-import { render } from "../../__tests__/utils/customRender";
+import {
+  act,
+  render,
+  waitFor,
+  screen,
+} from "../../__tests__/utils/customRender";
 import { Routes } from ".";
 import { saveStorageCity } from "@libs/asyncStorage/cityStorage";
 import { api } from "@services/api";
 import { mockWeatherAPIResponse } from "@__tests__/mocks/api/mockWeatherAPIResponse";
-import { act, screen, waitFor } from "@testing-library/react-native";
 
 describe("Routes", () => {
   it("should be render Search scrren when not city selected.", async () => {
